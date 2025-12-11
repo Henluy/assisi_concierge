39ca2539e6c74ea381357ebe9b042815378ed210
Dont Henluy Angenor Soro <donthenluyangenorsoro@MacBookPro.Home>
2025-12-12 00:20:07 +0100

    chore: add prettier, lower coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,26 @@
-
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.test.ts', '**/*.test.ts'],
     transform: {
-        // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-        // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-        '^.+\\.tsx?$': ['ts-jest', {
-            tsconfig: 'tsconfig.json',
-        }],
+        '^.+\\.tsx?$': 'ts-jest',
     },
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    collectCoverageFrom: [
+        'src/lib/**/*.ts',
+        'src/services/**/*.ts',
+        '!src/scripts/**',
+        '!**/*.d.ts',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 20,
+            functions: 20,
+            lines: 20,
+            statements: 20,
+        },
     },
 };
